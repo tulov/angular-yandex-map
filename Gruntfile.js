b164b2299169817d7a2a6b6039f0567ad8a4287d
tulov <tulov_alex@mail.ru>
2013-11-13 20:29:39 +0600

    Added min version ya-map and grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'ya-map.min.js': ['example/ya-map.js']
+                }
+            }
+        }
+    });
+
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+};
